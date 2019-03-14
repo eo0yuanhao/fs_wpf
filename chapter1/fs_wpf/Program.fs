f8cc4bed3d1277ead7f1ea09977aec7457e880b7
eo0yuanhao <eo0yuanhao@163.com>
2019-03-14 15:05:56 +0800

    chapter1 commit

---
+++ chapter1/fs_wpf/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+//[<EntryPoint>]
+//let main argv = 
+//    //printfn "%A" argv
+//    0 // return an integer exit code
+open Wpf_view
+[<STAThread>]
+do
+    Application().Run (window)    |> ignore
---
+++ chapter1/fs_wpf/wpf_view.fs
@@ -0,0 +1,9 @@
+﻿module Wpf_view
+open System
+open System.Windows
+open System.Windows.Controls
+
+let window:Window = Application.LoadComponent(new Uri("/MainWindow.xaml",UriKind.Relative)) |> unbox
+let button1:Button = window.FindName("button1") |> unbox
+let input1:TextBox = window.FindName("input1") :?> TextBox
+let tip1: Label = window.FindName("tip1") :?> Label
