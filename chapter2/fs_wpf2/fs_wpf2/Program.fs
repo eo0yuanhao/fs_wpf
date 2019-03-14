30d78b391d25a454212c7843da6f5738707c5f3b
eo0yuanhao <eo0yuanhao@163.com>
2019-03-14 21:55:11 +0800

    chapter 2

---
+++ chapter2/fs_wpf2/fs_wpf2/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+open System.Windows
+open System.Windows.Controls
+
+module ui = Wpf_view.NamedUiElement
+//[<EntryPoint>]
+[<STAThread>]
+//let main argv = 
+    //printfn "%A" argv    
+    //0 // return an integer exit code
+do      
+    let win=ui.window
+    ui.button1.Click.Add (fun x-> ui.tip1.Content <- ui.textbox.Text)
+    Application().Run win |> ignore
+
---
+++ chapter2/fs_wpf2/fs_wpf2/wpf_view.fs
@@ -0,0 +1,9 @@
+﻿namespace Wpf_view
+module NamedUiElement=
+    open System.Windows.Controls
+    open WpfApp1
+
+    let window:MainWindow= WpfApp1.MainWindow() |> unbox
+    let button1:Button= window.FindName("button1") |> unbox
+    let textbox = window.FindName("textbox") :?> TextBox
+    let tip1 = window.FindName("tip1") :?> Label
